b188d06a1104e281f284caf2b8383cdd0a0c264c
Bhupendrapatel98 <bhupendrapatel.bp57@gmail.com>
2020-05-05 15:43:01 +0530

    get data in sqlite table

--- app/src/main/java/com/example/sqlite/MainActivity.java
+++ app/src/main/java/com/example/sqlite/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.sqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,26 @@ public class MainActivity extends AppCompatActivity {
         //access MyHelper class
         MyHelper myHelper = new MyHelper(this);
         SQLiteDatabase database = myHelper.getReadableDatabase(); //here is two methods getReadableDatabase() and WriteableDatabase
+
+        //cursor object for get data
+        Cursor cursor = database.rawQuery("SELECT NAME,PRICE FROM PRODUCTS", new String[]{});
+
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+        StringBuilder builder = new StringBuilder(); // store all data(optional)
+        do {
+
+            String name = cursor.getString(0);
+            double price = cursor.getDouble(1);
+
+            builder.append("name" + name + "price" + price);
+
+            Log.i("msjdhsjk", "onCreate1: " + name);
+            Log.i("msjdhsjk", "onCreate2: " + price);
+            Log.i("msjdhsjk", "onCreate3: " + builder.toString());
+
+        } while (cursor.moveToNext());
 
     }
 }
